ded3be105b8e063381618c773587a875d29ceb82
MANISH D SALIAN <manishds2004@gmail.com>
2025-08-03 09:52:52 +0530

    Update index.js

--- server_side/index.js
+++ server_side/index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-mongoose.connect("mongodb://localhost:27017/employees")
+mongoose.connect("mongodb url/employees")
 
 app.post('/Register',(req,res)=>
         EmployeeModel.create(req.body)
